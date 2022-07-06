929747447d4d015e580928d89d858e9d89c13231
offAlex <off.sokal.al@gmail.com>
2022-07-07 02:52:02 +0300

    gameScene

---
+++ Assets/Scripts/cam.cs
@@ -0,0 +1,19 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class cam : MonoBehaviour
+{   
+    public Camera _camera;
+    [SerializeField] private Transform _player;
+ 
+    /// <summary>
+    /// Start is called on the frame when a script is enabled just before
+    /// any of the Update methods is called the first time.
+    /// </summary>
+    private void Start()
+    {   
+        _player = GameObject.FindGameObjectWithTag("Player").GetComponent<Transform>();
+        _camera.transform.SetParent(_player.transform);
+    }
+}
